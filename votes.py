5e68197ec8640b577c7eefa40ac335dc69bbb123
danielasay <59574167+danielasay@users.noreply.github.com>
2020-12-14 16:01:43 -0800

    Add files via upload

---
+++ votes.py
@@ -0,0 +1,11 @@
+votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie']
+
+jake_votes = votes.count('Jake')
+
+print(jake_votes)
+
+laurie_votes = print(votes.count('Laurie'))
+
+cassie_votes = print(votes.count('Cassie'))
+
+print("The winner is Jake!")
